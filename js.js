7ac411d24f1e55c6f3a5703bc86dbf728fdcb38e
codehemanth <152589429+codehemanth@users.noreply.github.com>
2024-03-24 00:06:51 +0530

    class1

---
+++ js.js
@@ -0,0 +1,15 @@
+console.log("Hemanth is learing javascript")
+
+const accountId = 252525
+var accountName = "Hemanth"
+let accountEmail = "chemanthgopal@gmail.com"
+accountPassword= "Jaipur" //never use this way of decleration
+let accountstate;
+
+// prefer not to use "var" because of block scope and functional scope
+/*
+multi comment useage
+*/
+
+console.log("account id is ",accountId)
+console.table([accountEmail,accountId,accountName,accountPassword,accountstate])
