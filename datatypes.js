df0e26e33dc08800ad90ccd560870735cce720b0
codehemanth <152589429+codehemanth@users.noreply.github.com>
2024-03-24 00:54:37 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,20 @@
+"use strict";// to run all scripts in newer version
+
+// alert( 3+3 ) // we are not using browser, we are using nodejs
+
+console.log(3+3);// always maintain code readability
+
+let name ="hitesh"
+let age = 18
+let isLoggedIn =false
+let state=null
+//number => 2 to the power 53
+// bigint // string => "  "// boolean=> ture or false
+// null => standalone value
+// undefined =>
+// symbol => unique
+//object
+
+console.log(typeof" hemanth ");
+console.log(typeof null);
+console.log(typeof undefined);
