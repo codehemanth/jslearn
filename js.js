0b36cb9b99b439aaae0fecf246715638860e06db
codehemanth <152589429+codehemanth@users.noreply.github.com>
2024-03-24 00:19:09 +0530

    reserved words and variable decleration

--- js.js
+++ js.js
@@ -1,5 +1,33 @@
 console.log("Hemanth is learing javascript")
 
+/*
+Names can contain letters, digits, underscores, and dollar signs.
+Names must begin with a letter.
+Names can also begin with $ and _ (but we will not use it).
+Names are case sensitive (y and Y are different variables).
+Reserved words (like JavaScript keywords) cannot be used as names.
+
+
+Rseserved Words
+
+abstract	arguments	await*	boolean
+break	byte	case	catch
+char	class*	const*	continue
+debugger	default	delete	do
+double	else	enum*	eval
+export*	extends*	false	final
+finally	float	for	function
+goto	if	implements	import*
+in	instanceof	int	interface
+let*	long	native	new
+null	package	private	protected
+public	return	short	static
+super*	switch	synchronized	this
+throw	throws	transient	true
+try	typeof	var	void
+volatile	while	with	yield
+*/
+
 const accountId = 252525
 var accountName = "Hemanth"
 let accountEmail = "chemanthgopal@gmail.com"
