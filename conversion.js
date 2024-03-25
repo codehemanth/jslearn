d2bdcdb3e56783f84a0402f38af21c2c129ed712
codehemanth <152589429+codehemanth@users.noreply.github.com>
2024-03-25 21:55:23 +0530

    conv

---
+++ conversion.js
@@ -0,0 +1,15 @@
+let score=33
+console.log(typeof score);
+let mad="chg"
+let tap=true
+let hem
+console.log(String(score));
+console.log(Number(mad))
+console.log(Number(tap))
+console.log(Boolean(mad))
+
+console.log(Boolean(mad))
+
+console.log(Boolean(hem))
+console.log(Number(hem))
+console.log(String(hem));
